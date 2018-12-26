a6cd4974e1b44c6c9f9cb9cf0848b511f9d7aef0
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-12-26 20:18:02 +0000

    sign up form and controllers completed

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -1,2 +1,18 @@
 class ReadersController < ApplicationController
+
+  get '/login' do
+    erb :'/readers/login'
+  end
+
+  post '/login' do
+
+  end
+
+  get '/signup' do
+    erb :'/readers/signup'
+  end
+
+
+
+
 end
