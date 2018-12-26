f9917140d960c00cf60d9eacdcf048fc438a1e4a
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-12-26 23:29:19 +0000

    views and controllers for /books/new and created ratings system

--- app/controllers/books_controler.rb
+++ app/controllers/books_controler.rb
@@ -10,21 +10,33 @@ class BooksController < ApplicationController
   end
 
   get '/books/new' do
-    
+
     if session[:id]
       erb :'/books/new'
     else
-      erb :'/login'
+      erb :'/readers/login'
     end
   end
 
-  get '/books/:id' do
+  get '/books/:slug' do
     if session[:id]
-      @reader = Reader.find(session[:id])
+      @book = Book.find_by_slug(params[:slug])
       erb :'/books/show'
     else
       erb :'/readers/login'
     end
   end
 
+  post '/books' do
+    if session[:id]
+      if Book.find_by(name: params[:name])
+        redirect '/books'
+      end
+      @book = Book.create(name: params[:name], author: params[:author], description: params[:description])
+      @book.owner = Reader.find(session[:id])
+      @book.rating = @book.add_new_rating(params[:rating].to_f)
+      redirect '/books'
+    end
+    redirect '/login'
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,11 +3,25 @@ class Book < ActiveRecord::Base
   belongs_to :community
   has_many :readers, through: :reader_books
 
+  attr_reader :all_ratings
+
+  def initialize(*args)
+    super
+    @all_ratings = Array.new
+  end
+
+  def add_new_rating(rating)
+    @all_ratings << rating
+    sum = 0.0
+    @rating = @all_ratings.map{|rating| sum += rating}[0] / @all_ratings.size.to_f
+    @rating
+  end
+
   def slug
     name.downcase.tr(' ', '-')
   end
 
   def self.find_by_slug(slug)
-    Song.all.find { |song| song.slug == slug }
+    Book.all.find { |book| book.slug == slug }
   end
 end
