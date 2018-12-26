d57274975527075f9c35aa6cf884ccbb2bb0e6b2
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-12-26 08:20:45 +0000

    sucsefully ran db:migrate

--- db/migrate/20181226074920_create_neighbourhoods.rb
+++ db/migrate/20181226074920_create_neighbourhoods.rb
@@ -1,8 +1,7 @@
 class CreateNeighbourhoods < ActiveRecord::Migration
   def change
     create_table :neighbourhoods do |t|
-      t.integer :user_id
-      t.integer :book_id
-    end 
+      t.string :name
+    end
   end
 end
--- db/migrate/20181226074933_create_reader_books.rb
+++ db/migrate/20181226074933_create_reader_books.rb
@@ -2,7 +2,7 @@ class CreateReaderBooks < ActiveRecord::Migration
   def change
     create_table :reader_books do |t|
       t.integer :user_id
-      t.integebr :book_id
-    end 
+      t.integer :book_id
+    end
   end
 end
