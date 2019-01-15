4d82b92cf6ea6d2dd90078d7bde6a7d60cbf2b73
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2019-01-15 21:04:22 +0000

    user validaton for books delete route

--- app/controllers/books_controler.rb
+++ app/controllers/books_controler.rb
@@ -96,6 +96,11 @@ class BooksController < ApplicationController
 
   delete '/books/delete/:slug' do
     if session[:id]
+
+      if Book.find_by_slug(params[:slug]).reader != Reader.find(session[:id])
+          redirect '/books'
+      end
+
       book = Book.find(session[:id])
       book.delete
       redirect '/books'
