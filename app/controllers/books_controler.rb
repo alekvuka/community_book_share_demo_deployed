7aa649e1661453330f3728ed52e7173c6d417989
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2019-01-15 22:24:07 +0000

    validating uniqueness of a new book not based on Book but Reader.community

--- app/controllers/books_controler.rb
+++ app/controllers/books_controler.rb
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
         redirect '/books/new'
       end
 
-      Books.all.each do |book|
+      Reader.find(session[:id]).community.books do |book|
         if book.slug == params[:name].downcase.tr(' ', '-')
           redirect '/books/new'
         end
