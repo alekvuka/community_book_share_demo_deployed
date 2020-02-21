f4a0647e0c247ab1b4e7cbd96b214a436b86ae1c
github username <alekvuka@gmail.com>
2020-02-20 21:22:57 -0500

    changed database and adapters to pg

---
+++ db/migrate/20200221015448_create_books.rb
@@ -0,0 +1,15 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+     t.string :author
+     t.string :name
+     t.string :description
+     t.float :rating
+     t.string :comments
+     t.integer :reader_id
+     t.integer :community_id
+     t.float :all_ratings
+     t.float :number_of_ratings
+   end
+  end
+end
